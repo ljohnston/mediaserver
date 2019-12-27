2b1caccd4696930e8222f1385c82ac05e4a4347a
Lance Johnston <johnston.lance@gmail.com>
2019-12-27 16:47:06 -0700

    Create photosync app tests

--- test/test_mediaserver_photosyncapp_sync.py
+++ test/test_mediaserver_photosyncapp_sync.py
@@ -32,6 +32,7 @@ class TestMediaserverPhotosyncSync(unittest.TestCase):
         print(self.test_dir)
         # shutil.rmtree(self.test_dir)
 
+
     def create_test_data(self, data_defs):
         # Filenames: YYYY-MM-DD_HH-MM-SS_IMG_<randomname>.<ext>
 
@@ -45,10 +46,21 @@ class TestMediaserverPhotosyncSync(unittest.TestCase):
                 filename = '{}_01-23-45_IMG_{:04d}.JPG'.format(data_def['files_prefix'], i)
                 fq_filename = os.path.join(path, filename)
                 with open(fq_filename, 'w') as f:
-                    f.write(filename)
+                    content = data_def.get('content', filename)
+                    f.write(content)
+
+
+    def delete_directory_contents(self, dir):
+        for root, dirs, files in os.walk(dir):
+            for f in files:
+                os.unlink(os.path.join(root, f))
+            for d in dirs:
+                shutil.rmtree(os.path.join(root, d))
 
 
     def test_sync_photos(self):
+
+        # Note duplicate content.
         data_defs = [
             {
                 'source_subdir': "Lance Johnston's iPhone",
@@ -57,9 +69,21 @@ class TestMediaserverPhotosyncSync(unittest.TestCase):
                 'files_end': 10},
             {
                 'source_subdir': "Lance Johnston's iPhone",
+                'files_prefix': '2019-01-02',
+                'files_start': 11,
+                'files_end': 11,
+                'content': 'foo'},
+            {
+                'source_subdir': "Lance Johnston's iPhone",
                 'files_prefix': '2019-01-03',
                 'files_start': 1,
-                'files_end': 10}
+                'files_end': 10},
+            {
+                'source_subdir': "Lance Johnston's iPhone",
+                'files_prefix': '2019-01-03',
+                'files_start': 11,
+                'files_end': 11,
+                'content': 'foo'},
             ]
         self.create_test_data(data_defs)
 
@@ -79,7 +103,69 @@ class TestMediaserverPhotosyncSync(unittest.TestCase):
                 if not os.path.isdir(f)]
         destination_dirs = [f for f in os.listdir(self.dest_photos_dir)]
 
-        self.assertEqual(len(synced_files), 20)
+        self.assertEqual(len(synced_files), 21)
+        self.assertEqual(len(destination_dirs), 2)
+        self.assertEqual(response.returncode, 0)
+
+
+    def test_sync_photos_with_deleted_photo(self):
+
+        data_defs = [
+            {
+                'source_subdir': "Lance Johnston's iPhone",
+                'files_prefix': '2019-01-02',
+                'files_start': 1,
+                'files_end': 10},
+            {
+                'source_subdir': "Lance Johnston's iPhone",
+                'files_prefix': '2019-01-03',
+                'files_start': 1,
+                'files_end': 10},
+            ]
+        self.create_test_data(data_defs)
+
+        source_dirs = set()
+
+        for data_def in data_defs:
+            source_dirs.add(os.path.join(self.source_photos_dir, data_def['source_subdir']))
+
+        args = [os.path.join(script_path, 'sync_photos'),
+                '-t', 'photosync-app',
+                '-s', ','.join(source_dirs),
+                '-d', self.dest_photos_dir]
+        response = subprocess.run(args, shell=False)
+
+        # We'll simulate a delete by removing the sources and recreating with
+        # one less song.
+
+        self.delete_directory_contents(self.source_photos_dir)
+
+        data_defs = [
+            {
+                'source_subdir': "Lance Johnston's iPhone",
+                'files_prefix': '2019-01-02',
+                'files_start': 1,
+                'files_end': 10},
+            {
+                'source_subdir': "Lance Johnston's iPhone",
+                'files_prefix': '2019-01-03',
+                'files_start': 1,
+                'files_end': 9},
+            ]
+        self.create_test_data(data_defs)
+
+        args = [os.path.join(script_path, 'sync_photos'),
+                '-t', 'photosync-app',
+                '-s', ','.join(source_dirs),
+                '-d', self.dest_photos_dir]
+        response = subprocess.run(args, shell=False)
+
+        synced_files = [
+                f for f in glob.glob(os.path.join(self.dest_photos_dir, '**/*'), recursive=True) \
+                if not os.path.isdir(f)]
+        destination_dirs = [f for f in os.listdir(self.dest_photos_dir)]
+
+        self.assertEqual(len(synced_files), 19)
         self.assertEqual(len(destination_dirs), 2)
         self.assertEqual(response.returncode, 0)
 
